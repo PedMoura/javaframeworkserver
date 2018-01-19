f72cef150c09086d8e1c2d80738ed032092eda7a
PedMoura <pedjmoura@hotmail.com>
2018-01-19 11:30:19 +0000

    deleted some unused checks

--- src/main/java/server/DynamicRouteLoader.java
+++ src/main/java/server/DynamicRouteLoader.java
@@ -28,16 +28,7 @@ public class DynamicRouteLoader {
                     throw new ClassNotFoundException("", e);
                 }
             }
-            if (name.equals("loadfromjar.Class1")) {
-                try {
-                    InputStream is = DynamicRouteLoader.class.getClassLoader().getResourceAsStream("loadfromjar/Class1.class");
-                    byte[] buf = new byte[10000];
-                    int len = is.read(buf);
-                    return defineClass(name, buf, 0, len);
-                } catch (IOException e) {
-                    throw new ClassNotFoundException("", e);
-                }
-            }
+           
             return getParent().loadClass(name);
         }
         public Class<?> loadCustomClass(String name) throws ClassNotFoundException, Exception {
@@ -79,16 +70,10 @@ public class DynamicRouteLoader {
 	
 	//public static Object loader() throws Exception {
 	public static Class<?> Loader(String classtoload) throws Exception {
-	    Class<?> cls;
+	    Class<?> cls = null;
 	    
 		if(classtoload == "ResponseClass") {
 			cls = new TestClassLoader().loadClass("server.ResponseClass");
-		}else {
-			if(classtoload == "loadfromjar.Class1") {
-				cls = new TestClassLoader().loadClass(classtoload);
-			}else {
-				cls = new TestClassLoader().loadClass("");
-			}
 		}
 	    System.out.println("---------------------------------------------loaded method------------------------------------");
 	    return cls;
